f5d4e45ee399312d51019ee424e000d9f0451092
unknown <arttujo@gmail.com>
2020-02-10 02:49:05 +0200

    changes to point handler

--- Server/Utils/querys.js
+++ Server/Utils/querys.js
@@ -90,95 +90,58 @@ const removePoint = (data,res) =>{
     )
 }
 
+//Helper function to handle updates when adding points to players
+const exec = (points,data,res,clickAmount)=> {
+    const selectQ = 'SELECT name,points FROM players WHERE `name` = ?'
+    const addPointsQ = 'UPDATE players SET points = points + '+ points +' WHERE `name` = ?'
+    db.connect().execute(
+        addPointsQ,
+        data,
+        (err,results,fields)=>{
+            if (err==null){
+                db.connect().query(
+                    selectQ,
+                    data,
+                    (err,results,fields)=>{
+                        //res.send(results)
+                        if (results[0].points === 0){
+                            let responseData= {
+                                response: "No more points",
+                                pointsEnded: true
+                            }
+                            res.send(responseData)
+                        } else {
+                            let toNextReward = 10-(clickAmount%10)
+                            let responseData = {
+                                name: results[0].name,
+                                points: results[0].points,
+                                clicksToNextReward: toNextReward
+                            }
+                            res.send(responseData)
+                        }
+                    }
+                )
+            }
+        }
+    )
+}
 
+//Not pretty but does all the handling related to points the user has
 const pointHandler = (data,res,clickAmount) =>{
     const selectQ = 'SELECT name,points FROM players WHERE `name` = ?'
     const rmOnePointQ = 'UPDATE players SET points = points - 1 WHERE `name` = ?'
-    const add5PointsQ = 'UPDATE players SET points = points + '+ 5 +' WHERE `name` = ?'
-    const add40PointsQ = 'UPDATE players SET points = points + '+ 40 +' WHERE `name` = ?'
-    const add250PointsQ = 'UPDATE players SET points = points + '+ 250 +' WHERE `name` = ?'
-
     if (clickAmount%500 == 0){
         console.log("500th")
-        db.connect().execute(
-            add250PointsQ,
-            data,
-            (err,results,fields)=>{
-                if (err==null){
-                    db.connect().query(
-                        selectQ,
-                        data,
-                        (err,results,fields)=>{
-                            res.send(results)
-                        }
-                    )
-                }
-            }
-        )
-
-       
+        exec(250,data,res,clickAmount)
     } else if(clickAmount%100 == 0){
         console.log("100th")
-        db.connect().execute(
-            add40PointsQ,
-            data,
-            (err,results,fields)=>{
-                if (err==null){
-                    db.connect().query(
-                        selectQ,
-                        data,
-                        (err,results,fields)=>{
-                            res.send(results)
-                        }
-                    )
-                }
-            }
-        )
-
+        exec(40,data,res,clickAmount)
     } else if(clickAmount%10 == 0){
         console.log("10th")
-        db.connect().execute(
-            add5PointsQ,
-            data,
-            (err,results,fields)=>{
-                if (err==null){
-                    db.connect().query(
-                        selectQ,
-                        data,
-                        (err,results,fields)=>{
-                            res.send(results)
-                        }
-                    )
-                }
-            }
-        )
-
+        exec(5,data,res,clickAmount)
     } else {
         console.log("nothing")
-        db.connect().execute(
-            rmOnePointQ,
-            data,
-            (err,results,fields)=>{
-                if (err==null){
-                    db.connect().query(
-                        selectQ,
-                        data,
-                        (err,results,fields)=>{
-                            if (results[0].points === 0){
-                                console.log("No more points")
-                                let responseData= {
-                                    response: "No more points",
-                                    pointsEnded: true
-                                }
-                                res.send(responseData)
-                            } else {
-                                res.send(results)
-                            }
-                        }
-                    )
-                }
-            }
-        )
+        exec(-1,data,res,clickAmount)
     }
 }
 
