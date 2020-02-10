6e3bdc21997156b6fed1923d470d7f9821ee0759
unknown <arttujo@gmail.com>
2020-02-11 00:44:56 +0200

    ui ready and need make Get requests

--- Client/ClickerGame/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Client/ClickerGame/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,9 +5,15 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import org.json.JSONObject
+import retrofit2.http.HTTP
+import java.net.HttpURLConnection
 import java.util.*
 
 
@@ -15,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     private var uniqueID:String?= null
     private var PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
+    private var client = OkHttpClient()
 
     private lateinit var postData:JSONObject
 
@@ -28,11 +35,16 @@ class MainActivity : AppCompatActivity() {
             Log.d("DBG",getUUID(this))
 
             doAsync {
-
+                //TODO GET REQUEST FOR POINT HANDLING HERE
+                uiThread {
+                    //TODO CHANGE SCORE LABEL AND CLICKS TO LABEL HERE
+                }
             }
         }
 
     }
+
+
 
     //Returns UUID as a string
     private fun getUUID(ctx:Context):String{
