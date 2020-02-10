e4a89715a0e08197b770958fa4abc829bc3bf3c4
unknown <arttujo@gmail.com>
2020-02-10 18:42:35 +0200

    UUID generation added

--- Client/ClickerGame/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Client/ClickerGame/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,58 @@
 package com.example.myapplication
 
-import android.bluetooth.BluetoothAdapter
 import android.content.Context
-import android.net.wifi.WifiManager
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.telephony.TelephonyManager
-import android.text.style.TtsSpan
 import android.util.Log
-import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.json.JSONObject
-import java.lang.Exception
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var uniqueID:String?= null
+    private var PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
+
     private lateinit var postData:JSONObject
 
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createUUID(this)
 
         playButton.setOnClickListener {
+            Log.d("DBG",getUUID(this))
+
             doAsync {
 
-                try {
-                    //var deviceName = Settings.Global.getString(contentResolver,Settings.Global.DEVICE_NAME)
-                    //Log.d("DBG",deviceName)
-                    //var deviceBtName = BluetoothAdapter.getDefaultAdapter()
-                    //Log.d("DBG",deviceBtName.name)
-                    var manager:TelephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-                    var imei = manager.imei
-                    Log.d("DBG",imei)
-
-                    postData.put("name", "test")
-                } catch (e:Exception){
-
-                }
             }
         }
 
     }
+
+    //Returns UUID as a string
+    private fun getUUID(ctx:Context):String{
+        val sharedPref = ctx.getSharedPreferences(PREF_UNIQUE_ID,Context.MODE_PRIVATE)
+        val uuid = sharedPref.getString(PREF_UNIQUE_ID,uniqueID)
+        return uuid!!
+    }
+    //Creates an UUID to identify the client on the server. UUID is store in the SQL database name field
+    private fun createUUID(ctx:Context){
+        if(uniqueID == null){
+            val sharedPrefs: SharedPreferences = ctx.getSharedPreferences(PREF_UNIQUE_ID,Context.MODE_PRIVATE)
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID,null)
+
+            if (uniqueID==null){
+                uniqueID = UUID.randomUUID().toString()
+                val editor = sharedPrefs.edit()
+                editor.putString(PREF_UNIQUE_ID,uniqueID)
+                editor.apply()
+            }
+        }
+    }
+
 }
